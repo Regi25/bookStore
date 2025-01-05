93576161e5be307833ae1dd01f925adff63c554a
regina <reginaiuldasheva@gmail.com>
2025-01-05 17:30:03 +0100

    Updates

---
+++ _next/static/Ym0QL7_4HMIxUtjmrRYKM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/AddBook":["static/chunks/pages/AddBook-fbc4d1ab6f8df69d.js"],"/_error":["static/chunks/pages/_error-9a890acb1e81c3fc.js"],sortedPages:["/AddBook","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
